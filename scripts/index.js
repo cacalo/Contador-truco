4260b583f4d601c295a05f23501960eae4cab3f8
Gonzalo Bechara <60068962+cacalo@users.noreply.github.com>
2022-12-25 17:18:45 -0300

    Botón de Reset con modal del aseguramiento

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,12 @@
 import { Contador } from "./contador.js";
 
+//Variables y selectores
 const partidaEnMemoria = JSON.parse(localStorage.getItem("truco"));
 const p1Nombre= "Nosotros";
 const p2Nombre= "Ellos";
+const modal= document.querySelector("dialog");
 
+//Intanciamiento de contadores
 const p1 = new Contador(
   p1Nombre,
   document.getElementById("jugador1Container"),
@@ -15,12 +18,17 @@ const p2 = new Contador(
     partidaEnMemoria && partidaEnMemoria[p2Nombre] ? partidaEnMemoria[p2Nombre] : 0
 );
 
-document.querySelector("#reset").addEventListener("click", ()=> {
+//Funciones del reset y modal
+document.querySelector("#reset").addEventListener("click", ()=> modal.showModal())
+document.querySelector("#volver").addEventListener("click", ()=> modal.close())
+document.querySelector("#aceptar").addEventListener("click", ()=> {
     p1.reset();
     p2.reset();
+    modal.close()
 })
-document.querySelectorAll("button").forEach(boton => boton.addEventListener("click", guardarPartida))
 
+//Guardado de partida
+document.querySelectorAll("button").forEach(boton => boton.addEventListener("click", guardarPartida))
 function guardarPartida(){
     const partidaAGuardar = {
         [p1Nombre]: p1.numero,
